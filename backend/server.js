924f94262bbfe2d8d9d232e6e9d80edf45200cc1
ibrahimvid <ivdinli@gmail.com>
2020-07-21 13:31:40 -0700

    setting up database

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
+mongoose.connect('mongodb://localhost:27017/postlock', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function () {
+  console.log('MongoDB database connection established successfully');
+});
+
 // Server settings
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
